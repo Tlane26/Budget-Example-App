4f7a8cc1b1fe131e3492974c7262af8c80bf30bf
Tlanetzi Chavez <chaveztlanetzi14@gmail.com>
2024-03-11 19:30:12 +0100

    Initial Commit

---
+++ Budget SwiftData/Budget_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_SwiftDataApp.swift
+//  Budget SwiftData
+//
+//  Created by Tlanetzi Chavez Madero on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget SwiftData
+//
+//  Created by Tlanetzi Chavez Madero on 11/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
